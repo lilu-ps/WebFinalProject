0287819e9602bad9dc858de049e54faf7c427a53
lilu-ps <48617956+lilu-ps@users.noreply.github.com>
2021-02-14 04:06:18 +0400

    changes to back again -_-

--- CurrencyApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CurrencyApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
