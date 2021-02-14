9743a01882c533cfc51a5c71dd3ecc8285521bca
lilu-ps <48617956+lilu-ps@users.noreply.github.com>
2021-02-14 05:12:24 +0400

    load artists

--- js/Artist.js
+++ js/Artist.js
@@ -12,6 +12,29 @@ export default class extends Common {
             .then(artstsData => {
                 artstsData = artstsData.Artists
                 console.log(artstsData);
+                artstsData.response.forEach(e => {
+                    console.log(e);
+                    console.log(e.name);
+
+                    artists.innerHTML += `
+                    <div class="box">
+                        <div class="details">
+                            <div class="cover">
+                                <img src="images\image1.jpg" alt="" />
+                            </div>
+                            <div class="image">
+                                <img src="${e.image}" alt="" />
+                            </div>
+                            <div class="artist-info">
+                                <span itemprop="name" class="artist-name">${e.firstName} ${e.lastName}</span>
+                                <span itemprop="name" class="studio-name">SOMETHING STUD</span>
+                                <button type="button">View Profile</button>
+                            </div>
+                        </div>
+                    </div>
+                 `;
+
+                });
             })
     }
 
