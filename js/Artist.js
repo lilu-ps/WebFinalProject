cef0b3cbdf61126759ecc071f5e354b0e24bef0e
lilu-ps <48617956+lilu-ps@users.noreply.github.com>
2021-02-14 04:13:59 +0400

    index.html changed

--- js/Artist.js
+++ js/Artist.js
@@ -9,52 +9,52 @@ export default class extends AbstractView {
 
     pageElements() {
 
-        var request = new XMLHttpRequest();
-        var url ='`https://localhost:44382' + '/api/Artist/getTattooArtists';
-        console.log(url);
-        request.open('GET', url, true);
-        var artists = document.getElementById('artist-list');
-        request.onload = function () {
-            if (this.status >= 200 && this.status < 400) {
-                // Success!
-                //var result = JSON.parse(this.response);
-                console.log(this.respons);
+        //var request = new XMLHttpRequest();
+        //var url ='`https://localhost:44382' + '/api/Artist/getTattooArtists';
+        //console.log(url);
+        //request.open('GET', url, true);
+        //var artists = document.getElementById('artist-list');
+        //request.onload = function () {
+        //    if (this.status >= 200 && this.status < 400) {
+        //        // Success!
+        //        //var result = JSON.parse(this.response);
+        //        console.log(this.respons);
 
-                //result.response.forEach(e => {
-                //    console.log(e);
-                //    console.log(e.name);
+        //        //result.response.forEach(e => {
+        //        //    console.log(e);
+        //        //    console.log(e.name);
 
-                //    artists.innerHTML += `
-                //        <div class="box">
-                //            <div class="details">
-                //                <div class="cover">
-                //                    <img src="images\image1.jpg" alt="" />
-                //                </div>
-                //                <div class="image">
-                //                    <img src="${e.image}" alt="" />
-                //                </div>
-                //                <div class="artist-info">
-                //                    <span itemprop="name" class="artist-name">${e.firstName} ${e.lastName}</span>
-                //                    <span itemprop="name" class="studio-name">SOMETHING STUD</span>
-                //                    <button type="button">View Profile</button>
-                //                </div>
-                //            </div>
-                //        </div>
-                //     `;
+        //        //    artists.innerHTML += `
+        //        //        <div class="box">
+        //        //            <div class="details">
+        //        //                <div class="cover">
+        //        //                    <img src="images\image1.jpg" alt="" />
+        //        //                </div>
+        //        //                <div class="image">
+        //        //                    <img src="${e.image}" alt="" />
+        //        //                </div>
+        //        //                <div class="artist-info">
+        //        //                    <span itemprop="name" class="artist-name">${e.firstName} ${e.lastName}</span>
+        //        //                    <span itemprop="name" class="studio-name">SOMETHING STUD</span>
+        //        //                    <button type="button">View Profile</button>
+        //        //                </div>
+        //        //            </div>
+        //        //        </div>
+        //        //     `;
 
-                //});
-                //var resp = this.response;
-            } else {
-                // We reached our target server, but it returned an error
+        //        //});
+        //        //var resp = this.response;
+        //    } else {
+        //        // We reached our target server, but it returned an error
 
-            }
-        };
+        //    }
+        //};
 
-        request.onerror = function () {
-            // There was a connection error of some sort
-        };
+        //request.onerror = function () {
+        //    // There was a connection error of some sort
+        //};
 
-        request.send();
+        //request.send();
     }
 
 
